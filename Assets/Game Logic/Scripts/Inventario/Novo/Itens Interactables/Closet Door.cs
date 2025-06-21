beed7497c94ba40eedae96a664a0f94b4618bc09
Luiz <164922548+UzCaroco@users.noreply.github.com>
2025-06-20 21:16:58 -0300

    Armario de cozinha

---
+++ Assets/Game Logic/Scripts/Inventario/Novo/Itens Interactables/Closet Door.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using Fusion;
+using UnityEngine;
+
+public class ClosetDoor : NetworkBehaviour, IInteractable
+{
+    [SerializeField] NetworkObject portaDireita, portaEsquerda;
+
+    [Networked] NetworkBool isOpen { get; set; } = false;
+
+    public void RPC_OnInteractObject(Inven playerInventory)
+    {
+        if (!isOpen)
+        {
+            RPC_OpenDoor();
+        }
+        else
+        {
+            RPC_CloseDoor();
+        }
+    }
+
+    [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
+    public void RPC_OpenDoor()
+    {
+        if (isOpen) return;
+        isOpen = true;
+        portaDireita.transform.localRotation = Quaternion.Euler(0, 90, 0);
+        portaEsquerda.transform.localRotation = Quaternion.Euler(0, -90, 0);
+    }
+
+    [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
+    public void RPC_CloseDoor()
+    {
+        if (!isOpen) return;
+        isOpen = false;
+        portaDireita.transform.localRotation = Quaternion.Euler(0, 0, 0);
+        portaEsquerda.transform.localRotation = Quaternion.Euler(0, 0, 0);
+    }
+}
